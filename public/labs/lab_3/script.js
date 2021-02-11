8b67c96e0be6788e0d785b3c1e7e416468bddd71
Kaitlyn Vesterby <kvesterb@terpmail.umd.edu>
2021-02-11 11:21:56 -0500

    labs 2 and 3

--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -1,1 +1,33 @@
-/* Put your javascript in here */
+const pictures = carousel.querySelectorAll('li');
+const nodelistToArray = Array.apply(null, pictures);
+
+
+pictures.forEach((picture, i) =>{
+    picture.style.position = 'relative';
+    picture.insertAdjacentHTML('beforeend', `<span style="position:absolute;left:0;top:0">${i}</span>`);;
+})
+
+    /* configuration */
+    let width = 130; // image width
+    let count = 3; // visible images count
+
+    let list = carousel.querySelector('ul');
+    let listElems = carousel.querySelectorAll('li');
+
+    let position = 0; // ribbon scroll position
+
+    carousel.querySelector('.prev').onclick = function() {
+      // shift left
+      position += width * count;
+      // can't move to the left too much, end of images
+      position = Math.min(position, 0)
+      list.style.marginLeft = position + 'px';
+    };
+
+    carousel.querySelector('.next').onclick = function() {
+      // shift right
+      position -= width * count;
+      // can only shift the ribbbon for (total ribbon length - visible count) images
+      position = Math.max(position, -width * (listElems.length - count));
+      list.style.marginLeft = position + 'px';
+    };
