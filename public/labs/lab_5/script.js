3ed08ff0e2102d5934cecd77df01e21f6a1ebc17
Kaitlyn Vesterby <kvesterb@terpmail.umd.edu>
2021-03-18 18:21:51 -0400

    panning and some css

--- public/labs/lab_5/script.js
+++ public/labs/lab_5/script.js
@@ -8,45 +8,6 @@ function mapInit() {
     zoomOffset: -1,
     accessToken: 'pk.eyJ1Ijoia3Zlc3RlcmJ5MjIiLCJhIjoiY2ttY3Fvcmg5MXMwbTJ3cDlha3g2ZXh0cSJ9.26HvIeYiNVZpVdmNDt2-vA'
   }).addTo(mymap);
-  const marker = L.marker([51.5, -0.09]).addTo(mymap);
-  const circle = L.circle([51.508, -0.11], {
-    color: 'red',
-    fillColor: '#f03',
-    fillOpacity: 0.5,
-    radius: 500
-  }).addTo(mymap);
-  const polygon = L.polygon([
-    [51.509, -0.08],
-    [51.503, -0.06],
-    [51.51, -0.047]
-  ]).addTo(mymap);
-
-  marker.bindPopup('<b>Hello world!</b><br>I am a popup.').openPopup();
-  circle.bindPopup('I am a circle.');
-  polygon.bindPopup('I am a polygon.');
-
-  // const popup = L.popup()
-  //   .setLatLng([51.5, -0.09])
-  //   .setContent('I am a standalone popup.')
-  //   .openOn(mymap);
-
-  // function onMapClick(e) {
-  //   alert(`You clicked the map at ${ e.latlng}`);
-  // }
-
-  // mymap.on('click', onMapClick);
-
-  // const popup = L.popup();
-
-  // function onMapClick(e) {
-  //   popup
-  //     .setLatLng(e.latlng)
-  //     .setContent(`You clicked the map at ${e.latlng.toString()}`)
-  //     .openOn(mymap);
-  // }
-
-  // mymap.on('click', onMapClick);
-
   return mymap;
 }
 async function dataHandler(mapObjectFromFunction) {
@@ -56,23 +17,30 @@ async function dataHandler(mapObjectFromFunction) {
 
   const request = await fetch('/api');
   const data = await request.json();
+  const coords = [];
 
   form.addEventListener('submit', async (event) => {
     event.preventDefault();
     console.log('form submitted');
-    const filtered = data.filter((record) => record.zip.includes(search.value)
+    if (search.value.length > 0) {
+      const filtered = data.filter((record) => record.zip.includes(search.value)
     && record.geocoded_column_1);
-    filtered.forEach((item) => {
-      const longLat = item.geocoded_column_1.coordinates;
-      console.log('markerLongLat', longLat[0], longLat[1]);
-      const marker = L.marker([longLat[1], longLat[0]]).addTo(mapObjectFromFunction);
-
-      const appendItem = document.createElement('li');
-      appendItem.classList.add('block');
-      appendItem.classList.add('list-item');
-      appendItem.innerHTML = `<div class="list-header is-size-5">${item.name}</div><address class="is-size-6">${item.address_line_1}</address><address class="is-size-6">${item.zip}</address>`;
-      targetList.append(appendItem);
-    });
+      filtered.forEach((item) => {
+        const longLat = item.geocoded_column_1.coordinates;
+        coords.push(longLat[0], longLat[1]);
+        console.log(coords);
+        console.log('markerLongLat', longLat[0], longLat[1]);
+        const marker = L.marker([longLat[1], longLat[0]]).addTo(mapObjectFromFunction);
+        const appendItem = document.createElement('li');
+        appendItem.classList.add('block');
+        appendItem.classList.add('list-item');
+        appendItem.innerHTML = `<div class="list-header is-size-5">${item.name}</div><address class="is-size-6">${item.address_line_1}</address><address class="is-size-6">${item.zip}</address>`;
+        targetList.append(appendItem);
+        mapObjectFromFunction.panTo([coords[1], coords[0]]);
+      });
+    } else {
+      targetList.append('');
+    }
   });
 }
 async function windowActions() {
